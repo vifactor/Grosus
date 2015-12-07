d35414acc7e5f8cbb2d6ae5b1e5a052691a82333
Viktor Kopp <vifactor@gmail.com>
2015-12-07 23:25:24 +0200

    Addeddummy index template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from app import grosus
+from flask import render_template
 
 @grosus.route('/')
 @grosus.route('/index')
 def index():
-    return "Hello, grosus"
+    user = {'nickname': 'Musterman'} # fake user
+    return render_template('index.html',
+                          title='Home',
+                          user=user)
