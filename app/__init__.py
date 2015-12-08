45d2c826012501a8549cdfa623b5594f8aec367e
Viktor Kopp <vifactor@gmail.com>
2015-12-08 22:05:45 +0200

    Basic scripts for database handling User model

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 grosus = Flask(__name__)
 grosus.config.from_object('config')
+db = SQLAlchemy(grosus)
 
-from app import views
+from app import views, models
--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'my-secret-key-value'
+
+# database stuff
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'grosus.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
