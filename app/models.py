1f912f37f78245cf849d7796c8959f441ed6d893
Viktor Kopp <vifactor@gmail.com>
2015-12-20 23:56:31 +0200

    Added new model Deputy.

--- app/models.py
+++ app/models.py
@@ -7,3 +7,15 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return '<User %r>' % self.login
+
+class Deputy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    group = db.Column(db.String(64), index=True)
+
+    def __repr__(self):
+        return '<Deputy %s (%s)>' % self.name, self.group
+    
+    def get_rating(user_id):
+        # FIXME
+        return 0
