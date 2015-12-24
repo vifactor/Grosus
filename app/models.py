08e8e89150ff1a3516242cfa376ee08d0ab10bc7
Viktor Kopp <vifactor@gmail.com>
2015-12-24 23:45:40 +0200

    Added simple Law model and defined author relationship between Law and Deputy

--- app/models.py
+++ app/models.py
@@ -8,6 +8,11 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return '<User %r>' % self.login
 
+authorship = db.Table('authorship',
+    db.Column('law_id', db.Integer, db.ForeignKey('law.id')),
+    db.Column('author_id', db.Integer, db.ForeignKey('deputy.id'))
+    )
+
 class Deputy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
@@ -19,3 +24,14 @@ class Deputy(db.Model):
     def get_rating(user_id):
         # FIXME
         return 0
+
+class Law(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(16))
+    title = db.Column(db.String(1024))
+    authors = db.relationship('Deputy',
+                            secondary = authorship,
+                            #primaryjoin=(followers.c.follower_id == id),
+                            #secondaryjoin=(followers.c.followed_id == id),
+                            backref=db.backref('laws', lazy='dynamic'),
+                            lazy='dynamic')
