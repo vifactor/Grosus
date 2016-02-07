82ca2d306b9750ede37978677313589002e7a224
Viktor Kopp <vifactor@gmail.com>
2016-02-07 23:12:24 +0200

    Added DeputyVote table to save deputy votings for laws Deputy appended with two methods accept(law) and reject(law) to vote

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from flask.ext.login import UserMixin
+from sqlalchemy import UniqueConstraint
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,11 +20,24 @@ class Deputy(db.Model):
     group = db.Column(db.String(64), index=True)
 
     def __repr__(self):
-        return '<Deputy %s (%s)>' % self.name, self.group
+        return '<Deputy %s (%s)>' % (self.name, self.group)
     
-    def get_rating(user_id):
+    def get_rating(self, user_id):
         # FIXME
         return 0
+    
+    def accept(self, law):
+        """Adds 'True' vote to the Deputy voting table"""
+        vote = DeputyVote(deputy_id = self.id, law_id = law.id, vote_option = True)
+        db.session.add(vote)
+        db.session.commit()
+        
+    def reject(self, law):
+        """Adds 'True' vote to the Deputy voting table"""
+        vote = DeputyVote(deputy_id = self.id, law_id = law.id, vote_option = False)
+        db.session.add(vote)
+        db.session.commit()
+
 
 class Law(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,3 +49,17 @@ class Law(db.Model):
                             #secondaryjoin=(followers.c.followed_id == id),
                             backref=db.backref('laws', lazy='dynamic'),
                             lazy='dynamic')
+    
+    def __repr__(self):
+        return '<Law %s: "%s">' % (self.code, self.title)
+
+from sqlalchemy import Enum
+class DeputyVote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    deputy_id = db.Column(db.Integer, db.ForeignKey('deputy.id'))
+    law_id = db.Column(db.Integer, db.ForeignKey('law.id'))
+    # True if law is accepted
+    # False if law is rejected
+    vote_option = db.Column(db.Boolean)
+    
+    __table_args__ = (UniqueConstraint('deputy_id', 'law_id', name='_deputy_vote_for_law'),)
